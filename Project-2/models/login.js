cbbd6392457ad3e65c890038306d07fa6eb5bdb6
MAX_BLOOM <bloommax3@gmail.com>
2019-10-16 10:35:43 -0400

    added login. Need to test and debug

--- Project-2/models/login.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var Login = sequelize.define("login", {
-    username: DataTypes.STRING,
-    passwork: DataTypes.TEXT
-  });
-  return Login;
-};
