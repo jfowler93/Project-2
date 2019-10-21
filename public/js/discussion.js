6b44b44dd2cd8e9b5bdb3cd08cbf947608f29c98
MAX_BLOOM <bloommax3@gmail.com>
2019-10-21 07:24:47 -0400

    working on update and delete

--- public/js/discussion.js
+++ public/js/discussion.js
@@ -22,4 +22,27 @@ $(document).ready(function() {
         })
     })
 
+    if($("."+localStorage.getItem("username"))){
+        console.log($(this).attr("id"))
+        $("."+localStorage.getItem("username")).append("<button type='button' class='btn aqua-gradient' id='update-button'>Update</button>   <button type='button' class='btn aqua-gradient' id='delete-button'>Delete</button>")
+    }
+
+    $(document).on("click", "#update-button", function(){
+        let row = $("<div class='row'>")
+        let form = $(`<div class="col-sm-8 col-sm-offset-2">
+        <label for="exampleFormControlTextarea5">Update your Review Here!</label>
+        <textarea class="form-control" id="update-form" rows="3"></textarea><button class="btn aqua-gradient" id="submit-update">Submit</button></div>`)
+        $(row).append(form)
+        $("."+localStorage.getItem("username")).append(row)
+    })
+
+    $(document).on("click", "#submit-update", function(){
+        var text = $("#update-form").val().trim()
+        
+    })
+
+    $(document).on("click", "#delete-button", function(){
+
+    })
+
 })
